d0fc18045f39fe67b75f9775b67e415b30901e8b
TNTzx <122166834+Uncubically@users.noreply.github.com>
2023-02-02 13:40:06 +0800

    switch

---
+++ backend/control_flow/conditional/switch.hpp
@@ -0,0 +1,62 @@
+#ifndef BACKEND_CONTROLFLOW_CONDITIONAL_SWITCH
+#define BACKEND_CONTROLFLOW_CONDITIONAL_SWITCH
+
+
+
+#include <iostream>
+#include <memory>
+
+#include "../../../menu/_init.hpp"
+
+#include "../../program_menu.hpp"
+#include "../../path.hpp"
+
+
+
+namespace Conditional {
+    class Switch : public ProgramMenu {
+        public:
+            Switch() : ProgramMenu() {
+                // Setting menu's title and description
+                this->title = File::read_str_file(backend_path + "control_flow/conditional/switch_title.txt");
+                this->desc = "Conditional multi-branching for branches > 2 using switch statements.";
+                // TODO creator
+            };
+
+
+            void run_program() override {
+                char letter;
+
+                std::cout << "What's your order ma'am/sir? (a / b / c): ";
+                std::cin >> letter;
+                std::cout << std::endl;
+
+                switch (letter) {
+                    case 'a':
+                        std::cout << "Apple";
+                        break;
+                    case 'b':
+                        std::cout << "Banana";
+                        break;
+                    case 'c':
+                        std::cout << "Coconut";
+                        break;
+                    default:
+                        std::cout << "Sorry, that's not in the menu.";
+                        break;
+                }
+            };
+    };
+
+	class SwitchChoice : public ConsMenu::Choice {
+        public:
+            SwitchChoice() : ConsMenu::Choice() {
+                this->description = "Switch";
+                this->menu = std::make_unique<Switch>(Switch());
+            };
+	};
+};
+
+
+
+#endif
